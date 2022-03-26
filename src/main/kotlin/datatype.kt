91da7f0148c9115d025588745a9f8f4bf22fb01c
= <redalsdn7@naver.com>
2022-03-26 19:55:13 +0900

    kotlin 실습

--- src/main/kotlin/datatype.kt
+++ src/main/kotlin/datatype.kt
@@ -1,8 +1,22 @@
 fun main() {
 
-    val name: String = "minu"
-    var age: Int = 0
-    age = 32
-    println("name $name, age $age")
+//    val name: String = "minu"
+//    var age: Int = 0
+//    age = 32
+//    println("name $name, age $age")
 
+//    val a = 1
+//    val str1 = "a = $a"
+//    val str2 = "a = ${a + 2}"
+//    println("str: \"$str1\",str2 : \"$str2\"")
+
+    var str1 : String?
+    str1 = null
+
+    val len = str1?.length ?: -1
+    println("str: $str1, length : $len")
+
+    var a : Any = 1
+    a = 20L
+    println(a.javaClass)
 }
